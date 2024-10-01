a61e16c8a6bca28ebf8cf3fa758fcebb76af4a34
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-10-01 12:07:06 +0200

    Some changes

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,7 +3,14 @@ import Async from "./Async";
 
 describe("Async Component", () => {
   //Fn uses async
-  test("render posts if request success", async () => {
+  test("render posts if request success mock data", async () => {
+    //create mock
+    window.fetch = jest.fn();
+    //using the mock
+    window.fetch.mockResolvedValueOnce({
+      //This simulate the real Obj structure returned by the real fetch :)
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
     render(<Async />);
     //because is a List
     //use await , now the test Fn is async
